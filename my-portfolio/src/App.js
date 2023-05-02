237a98801ba029e5a4204b829af3d37483092f64
pierr05 <pierreachlet322@gmail.com>
2023-05-01 23:03:12 -0400

    Added code to App.js file

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import { About } from './components/about';
+
 function App() {
-  return <Header />
+  return (
+    <div>
+    <Header />
+    <About />
+    </div>
+  ); 
 }
 
 export default App;
