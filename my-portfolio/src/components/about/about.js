efba07a589044285b30182ccf8724d6dacc8888d
pierr05 <pierreachlet322@gmail.com>
2023-05-01 23:03:49 -0400

    created about.js

---
+++ my-portfolio/src/components/about/about.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './about.css'
+
+function About(props) {
+    return (
+        <section className='about-me'>
+            <h1>About Me</h1>
+            <p className='first-letter'>
+            Hi, my name is Achlet Pierre. I am a front end developer with a background in customer service. Trained at University of Central Florida coding boot camp and certificated in full-stack web development. Creative problem-solver passionate about developing apps; focused on mobile-first design and development. Strengths in creativity, communication, teamwork, and building projects from start to finish.
+            </p>
+        </section>
+    ); 
+}
+
+export default About; 
