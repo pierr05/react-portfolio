3c6b23e1ae22e8575e83bd58814fa8288ca5ded4
pierr05 <pierreachlet322@gmail.com>
2023-05-01 23:04:34 -0400

    created header.js

---
+++ my-portfolio/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { Navigation } from "../Navigation";
+import './Header.css';
+
+const Header =(() => {
+    return (
+        <section className="header">
+            <h1 className="title">Achlet Pierre</h1>
+            <section className-navbar>
+                <Navigation />
+            </section>
+        </section>
+    );
+});
+
+export default Header; 
